da7d2ed2ad837e035fe291401c7670c4487c7351
onecastell <bloofrank55@gmail.com>
2018-12-28 22:10:01 -0500

    Code Cleanup: removed unessesary return statement

--- cash-register.js
+++ cash-register.js
@@ -102,9 +102,6 @@ function checkCashRegister(price, cash, cid) {
         return {
             "status": "INSUFFICIENT_FUNDS", "change": []
         };
-
-    //If no condition is fulfilled, return null
-    return null;
 }
 
 console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
